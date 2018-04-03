aed62eaac00c9a3d68d8327304324353ac84d335
vudaoanhtuan <vudaoanhtuan@gmail.com>
2018-04-03 23:13:27 +0700

    update question

--- js/question.js
+++ js/question.js
@@ -1,23 +1,84 @@
 var current = 0;
-var question = [];
+var question = [
+    {
+        cauhoi: "Tuần hoàn của TB công nghiệp là sự thống nhất của các hình thái tuần hoàn nào:",
+        a: "TB tiền tệ, TB sản xuất và TB sức lao động",
+        b: "TB sản xuất, TB sức lao động, TB tiền tệ",
+        c: "TB tiền tệ, TB sản xuất và TB hàng hóa",
+        d: "TB hàng hóa, TB sản xuất",
+        answer: "c"
+    },
+    {
+        cauhoi: "TB cố định là gì",
+        a: "Hình thức tồn tại là máy móc, thiết bị, nhà xưởng",
+        b: "Sức lao động",
+        c: "Hình thức tồn tại là nguyên liệu, nhiên liệu, vật liệu",
+        d: "Là bộ phận tư bản được hoàn lại toàn bộ cho các nhà TB sau mỗi quá trình sản xuất",
+        answer: "a"
+    },
+    {
+        cauhoi: "Thời gian chu chuyển của tư bản bao gồm",
+        a: "Thời gian lao động, thời gian mua bán hàng hóa",
+        b: "Thời gian sản xuất, thời gian lưu thông",
+        c: "A và B đúng",
+        d: "A và B sai",
+        answer: "b"
+    },
+    {
+        cauhoi: "TB lưu động là:",
+        a: "Nguyên liệu, nhiên liệu, vật liệu sản xuất",
+        b: "Sức lao động",
+        c: "A và B đúng",
+        d: "A và B sai",
+        answer: "c"
+    },
+    {
+        cauhoi: "Chu kì kinh tế bao gồm các giai đoạn là",
+        a: "Hưng thịnh, trì trệ, khủng hoảng, phục hồi",
+        b: "Ổn định, khủng hoảng, phục hồi, hưng thịnh",
+        c: "Ổn định, trì trệ, khủng hoảng, phục hồi",
+        d: "Khủng hoảng, tiêu điều, phục hồi, hưng thịnh",
+        answer: "d"
+    },
+    {
+        cauhoi: "Tổng sản phẩm xã hội được cấu thành bởi bao nhiêu bộ phận",
+        a: "2",
+        b: "3",
+        c: "4",
+        d: "5",
+        answer: "b"
+    },
+    {
+        cauhoi: "Nhận định nào đúng",
+        a: "TB cố định là TB bất biến",
+        b: "TB bất biến là TB cố định",
+        c: "A và B đúng",
+        d: "A và B sai",
+        answer: "a"
+    },
+    {
+        cauhoi: "Điều kiện thực hiện tổng sản phẩm xã hội trong tái sản xuất đơn giản là",
+        a: "I(v + m) = II(v + m)",
+        b: "II(v + m) = Ic",
+        c: "I(v + m) + II(c + m) = II(c + v + m)",
+        d: "I(c + v + m) = IIc + Ic",
+        answer: "d"
+    },
+    {
+        cauhoi: "Chu kỳ kinh tế của chủ nghĩa tư bản là khoảng thời gian nền kinh tế tư bản chủ nghĩa vận động từ...",
+        a: "cuối cuộc khủng hoảng này đến đầu khủng hoảng sau",
+        b: "đầu cuộc khủng hoảng này đến cuối khủng hoảng sau",
+        c: "đầu cuộc khủng hoảng này đến đầu khủng hoảng sau",
+        d: "cuối cuộc khủng hoàng này đến cuối khủng hoảng sau",
+        answer: "c"
+    },
+    {
+        cauhoi: "Giá trị của tổng sản phẩm xã hội được phân giải thành",
+        a: "C + V + M'",
+        b: "M + M'",
+        c: "C + V + M",
+        d: "C + V",
+        answer: "c"
+    }
 
-cau1 = {
-    cauhoi: "Một cộng một bằng mấy Năm aaaaa aaaaaa aaaa aaaaaa aaaa Một cộng một bằng mấy Một cộng một bằng mấy Một cộng một bằng mấy Một cộng một bằng mấyNăm aaaaa aaaaaa aaaa aaaaaa aaaa Một cộng một bằng mấy Một cộng một bằng mấy Một cộng một bằng mấy Một cộng một bằng mấyNăm aaaaa aaaaaa aaaa aaaaaa aaaa Một cộng một bằng mấy Một cộng một bằng mấy ",
-    a: "Hai",
-    b: "Ba",
-    c: "Bốn",
-    d: "Năm aaaaa aaaaaa aaaa aaaaaa aaaa Một cộng một bằng mấy Một cộng một bằng mấy Một cộng một bằng mấy Một cộng một bằng mấy",
-    answer: "a"
-};
-
-cau2 = {
-    cauhoi: "Một cộng ba bằng mấy",
-    a: "Hai",
-    b: "Ba",
-    c: "Bốn",
-    d: "Năm",
-    answer: "c"
-};
-
-question.push(cau1);
-question.push(cau2);
+];
